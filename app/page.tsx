71ce786129ed9ab6b5a31aa8576f18d6a2fa12db
Andrew-243 <aulloa2017@fau.edu>
2024-12-21 16:47:03 -0500

    updated opengraph image

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
-"use client";
-
+import { Metadata } from "next";
 import { LoginForm } from "@/components/login-form";
 import Image from "next/image";
+
+export const metadata: Metadata = {
+  openGraph: {
+    images: [
+      {
+        url: 'https://cdn.prod.website-files.com/6729490eec7b4529805b89b0/6752962dceb9162ce9abfa3e_Open%20Graph%20Image%20EN-US_v3.png', // Use your OG image URL here
+        width: 1200,
+        height: 630,
+        alt: 'Sun Locke',
+      },
+    ],
+  },
+};
 
 export default function Home() {
   return (
