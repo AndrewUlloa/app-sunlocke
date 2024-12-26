01d9e712284091f8dd0d87795f117db973b7aa64
Andrew-243 <aulloa2017@fau.edu>
2024-12-25 21:40:01 -0500

    Refactor button component to adjust default and large size padding and update button hover shadow

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
           "shadow-button-outer",
           "transition-all duration-200 ease-out",
           "hover:shadow-button-outer-hover hover:duration-[375ms]",
-          "active:shadow-button-outer-press active:scale-[0.98] active:duration-200",
+          "active:shadow-button-outer-press active:scale-[0.98] active:duration-100",
         ].join(" "),
         destructive:
           "bg-red-500 text-neutral-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-neutral-50 dark:hover:bg-red-900/90",
