747cf2fa80f3f2d5e4cff849a712fe63b3c6cc1a
Andrew-243 <aulloa2017@fau.edu>
2025-01-30 22:45:58 -0500

    Fix typo in Open Graph image URL in TranscribePage metadata

--- app/transcribe/page.tsx
+++ app/transcribe/page.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
     siteName: "Sun Locke",
     images: [
       {
-        url: 'hhttps://cdn.prod.website-files.com/6729490eec7b4529805b89b0/679c463ae979ac4ba80ff672_Open%20Graph%20Image%20EN_US_AI_Companion_v2.png',
+        url: 'https://cdn.prod.website-files.com/6729490eec7b4529805b89b0/679c463ae979ac4ba80ff672_Open%20Graph%20Image%20EN_US_AI_Companion_v2.png',
         width: 1200,
         height: 630,
         alt: 'Sun Locke',
