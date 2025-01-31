ac352b5cf12ec9b2e62d4b55c0d289318b583f25
Andrew-243 <aulloa2017@fau.edu>
2025-01-30 22:48:27 -0500

    Update Header component to wrap logo in a link for improved navigation

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function Header() {
   return (
     <section className="w-full max-w-[1192px] h-[76px] flex flex-row items-center justify-center py-6 mx-auto">
-        <div className="flex items-center justify-center">
+        <a href="https://www.sunlocke.com" className="flex items-center justify-center gap-[4px]">
           <Image
             src="https://cdn.prod.website-files.com/6729490eec7b4529805b89b0/672971cc952da991c5eaddb4_android-chrome-512x512.png"
             alt="Sun Locke Logo"
@@ -15,7 +15,7 @@ export function Header() {
           <div className="text-[28px] leading-[125%] font-tobiasSemiBold tracking-[-0.04em]">
             Sun Locke
           </div>
-        </div>
+        </a>
     </section>
   );
 }
